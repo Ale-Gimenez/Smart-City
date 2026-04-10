76fec4bedf5a15ddc6090f5ea11426e56fcc2e7d
Alejandra Gimenez <alemichgimenez@gmail.com>
2026-04-10 11:20:34 -0300

    Serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,47 +2,74 @@ from rest_framework import serializers
 from .models import Responsaveis, Locais, Ambientes, Microcontroladores, Sensores, Historicos, Usuarios
 from django.contrib.auth.models import User
 
-class UsuarioSerializer(serializers.ModelSerializer):
+class ResponsavesSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Usuario
+        model = Responsaveis
+        fields = '__all__'
+
+class LocaisSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Locais
+        fields = '__all__'
+
+class AmbientesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ambientes
+        fields = '__all__'
+
+class MicrocontroladoresSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Microcontroladores
+        fields = '__all__'
+
+class SensoresSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensores
+        fields = '__all__'
+
+class HistoricosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Historicos
+        fields = '__all__'
+
+class UsuariosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuarios
         fields = '__all__'
 
 class RegisterSerializer(serializers.Serializer):
     # Tabela auth_user
     username = serializers.CharField()
-    email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
     # Tabela api_usuario
     nome = serializers.CharField(required=False, allow_blank=True, default='')# não colocar espaço no meio das aspas, vai dar erro
     telefone = serializers.CharField(required=False, allow_blank=True, default='')
-    tipo = serializers.ChoiceField(choices=Usuario.TIPO_CHOICES)
+    tipo = serializers.ChoiceField(choices=Usuarios.TIPO_CHOICES)
 
     def create(self, validated_data):
         # Criando na tabela api_usuario
         nome = validated_data.get('nome', '')
-        email = validated_data['email']
         telefone = validated_data.get('telefone', '')
         tipo = validated_data['tipo']
 
         # Criando na tabela auth_user
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=email,
             password=validated_data['password']
         )
 
-        if tipo == "LOCADOR":
+        if tipo == "ADMINISTRADOR":
             user.is_staff = True
+            user.is_active = True
         else:
             user.is_staff = False
-        user.is_active = True
-        user.is_superuser = False
-        user.save()
+            user.is_active = True
+            user.is_superuser = False
+            user.save()
 
-        Usuario.objects.create(
-            user = user, # o da esquerda 
+        Usuarios.objects.create(
+            user = user,
             nome = nome if nome else user.username,
-            email = email,
             telefone = telefone,
             tipo = tipo
         )
@@ -51,27 +78,12 @@ class RegisterSerializer(serializers.Serializer):
     
 class UsuarioMeSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)
-    email = serializers.CharField(source='user.email', read_only=True)
 
     is_superuser = serializers.BooleanField(source='user.is_superuser', read_only=True)
     is_staff = serializers.BooleanField(source='user.is_staff', read_only=True)
     is_active = serializers.BooleanField(source='user.is_active', read_only=True)
 
     class Meta:
-        model = Usuario
-        fields = ['id', 'nome', 'email', 'telefone', 'tipo', 'username', 'is_superuser', 'is_staff', 'is_active']
+        model = Usuarios
+        fields = ['id', 'nome', 'telefone', 'tipo', 'username', 'is_superuser', 'is_staff', 'is_active']
 
-class ImovelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Imovel
-        fields = '__all__'
-
-class ContratoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contrato
-        fields = '__all__'
-
-class PagamentoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pagamento
-        fields = '__all__'
